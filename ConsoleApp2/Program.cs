da2e9abe61fdf2567f31d782d2989f288f6cc525
Aldijana Zahirović <aldijanazahirovic406@gmail.com>
2022-07-22 20:57:35 -0700

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+
+{
+    class Program
+    {
+        static void Main(string[] args) {
+        switch (args.Length) {
+        case 0:
+          Console.WriteLine("Nema proslijeđenih argumenata.");
+          break;
+        case 1:
+          Console.WriteLine("Registriran jedan argument.");
+          break;
+        case 2:
+          Console.WriteLine("Registrirana dva argumenta.");
+          break;
+        case 3:
+          Console.WriteLine("Registrirana tri argumenta.");
+          break;
+        default:
+          Console.WriteLine("Registrirana {0} argumenta.");
+          break;
+            }
+        }
+    }
+}
